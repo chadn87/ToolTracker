8e499d73f8a22ca1a84e5c4ca3c202a96e4eed30
chadn <chadn@G718>
2017-01-17 16:39:01 -0600

    Revert "Added sql database for test login. added basic login page."
    This reverts commit f6a3a2efbb01429d7c8d80dc823da48d08fea21b.

--- ToolTracker/ToolTracker/LoginPage.aspx.cs
+++ ToolTracker/ToolTracker/LoginPage.aspx.cs
@@ -4,36 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace ToolTracker
 {
     public partial class LoginPage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void login_button_Click(object sender, EventArgs e)
-        {
-            SqlConnection con = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\chadn\\Source\\Repos\\ToolTracker\\ToolTracker\\ToolTracker\\App_Data\\ToolTracker.mdf; Integrated Security = True");
-            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From Login Where userName='" + user_name.Text + "' and password='" + password.Text + "'", con);
-            DataTable dt = new DataTable();
-            sda.Fill(dt);
-
-            if(dt.Rows[0][0].ToString() == "1")
-            {
-                Response.Redirect("LoginSuccess.aspx");
-            }
-            else
-            {
-                invalid_login.Visible = true;
-            }
-        }
-
-        protected void cancel_button_Click(object sender, EventArgs e)
         {
 
         }
--- ToolTracker/ToolTracker/LoginPage.aspx.designer.cs
+++ ToolTracker/ToolTracker/LoginPage.aspx.designer.cs
@@ -56,14 +56,5 @@ namespace ToolTracker {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button login_button;
-        
-        /// <summary>
-        /// invalid_login control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label invalid_login;
     }
 }
